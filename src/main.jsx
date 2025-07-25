0e65262d8e3598f39c944338b1b46c1470799bf8
Fuegodevalecs <fuegoalecs@gmail.com>
2025-07-25 09:08:48 +0100

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -4,7 +4,7 @@ import ReactDOM from "react-dom/client";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query"; // if using React Query
 import { AlchemyAccountProvider } from "@account-kit/react";
 // main.jsx
-import { config } from "./config";
+import { config, uiConfig } from "./config";
 
 import App from "./App.jsx";
 
@@ -13,7 +13,7 @@ const queryClient = new QueryClient();
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
     <QueryClientProvider client={queryClient}>
-      <AlchemyAccountProvider config={config} queryClient={queryClient}>
+      <AlchemyAccountProvider config={config} uiConfig={uiConfig} queryClient={queryClient}>
         <App />
       </AlchemyAccountProvider>
     </QueryClientProvider>
